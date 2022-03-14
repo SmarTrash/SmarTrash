883b1784c04fb4a0b8c9e48577e62d9b249d5aa2
NogaBob <93701569+NogaBob@users.noreply.github.com>
2022-03-14 13:27:19 +0200

    Commit 14/03/22

--- SmarTrash/Data/Model1.Designer.cs
+++ SmarTrash/Data/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\noy\Desktop\SmarTrash\SmarTrash\SmarTrash\Data\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\noga1\OneDrive\שולחן העבודה\שנה ג\סמסטר א\פרוייקט גמר\SMARTRASH\SmarTrash\SmarTrash\Data\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
