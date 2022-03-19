d1169f12bfe853c9b9f91eee1eb220cfa96d45f9
NogaBob <93701569+NogaBob@users.noreply.github.com>
2022-03-19 14:56:17 +0200

    LastChangeGiftScreens

--- SmarTrash/SmarTrash-React/SmarTrash/src/screens/GiftScreen/NewGiftPage.jsx
+++ SmarTrash/SmarTrash-React/SmarTrash/src/screens/GiftScreen/NewGiftPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View, Text,TextInput, SafeAreaView, StyleSheet, Dimensions, Image } from 'react-native';
 import COLORS from '../../consts/colors';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import gifts from '../../consts/gifts';
 
 const width = Dimensions.get('screen').width/2-30;
-const NewGiftPage = () => {
+const NewGiftPage = ({navigation}) => {
   const categories =[ 'הכל', 'נוי','בית','פנאי','ביגוד'];
   const [categoryIndex, setCategoryIndex] = React.useState(0);
 
@@ -25,53 +25,39 @@ const NewGiftPage = () => {
           {item}
         </Text>
         </TouchableOpacity>
-      ) )}
+      ))}
     </View>
     );
   };
 const Card = ({gifts}) => {
   return (
-  <View style={style.card}>
-    <View style={{height:100, alignItems:'center'}}>
-      <Image  
-      style={{flex:1, resizeMode:'contain'}}
-      source={{
+    <TouchableOpacity onPress={() => navigation.navigate('NewSelectedGift',gifts)}>
+    <View style={style.card}>
+     <View style={{height:100, alignItems:'center'}}>
+        <Image  
+          style={{flex:1, resizeMode:'contain'}}
+          source={{
           uri: 'https://reactnative.dev/img/tiny_logo.png',
-        }} >
-      </Image>
-    </View>
-    <Text style={{
-      fontWeight:'bold', 
-      fontSize:17, 
-      marginTop:10,
-      textAlign:'center'}}>
-      {gifts.GiftName}
-    </Text>
-    <View style={{ 
-      justifyContent:'space-between',
-      marginTop:5
-      }}>
-    <Text style={{
-      fontWeight:'bold', 
-      fontSize:15, 
-      marginTop:10,
-      textAlign:'center'}}
-    >  
-     <Icon name="star" size={20} color={'#FFD700'} />
-    {gifts.Price}
-   
-    </Text>
-    </View>
+                }} >
+        </Image>
+      </View>
+      <Text style={style.giftName}>
+          {gifts.GiftName}
+      </Text>
+      <View style={{justifyContent:'space-between', marginTop:5}}>
+        <Text style={style.giftPrice}>  
+          <Icon name='star-circle'npm size={30} color='#FFD700' />
+              {gifts.Price}
+        </Text>
+      </View>
   </View>
-  )
+</TouchableOpacity>
+  
+  );
 };
    return (
     <SafeAreaView 
-      style={{
-        felx:1,
-        paddingHorizontal:20, 
-        backgroundColor:COLORS.white
-        }}>
+      style={style.safeArea}>
       <View style={style.header}>
         <View>
           <Text style={style.text}> הטבות </Text>
@@ -103,6 +89,11 @@ const Card = ({gifts}) => {
 };
 
 const style = StyleSheet.create({
+  safeArea: {
+    felx:1,
+    paddingHorizontal:20, 
+    backgroundColor:COLORS.white,
+  },
   header:{
     marginTop:30,
     justifyContent: 'space-between',
@@ -174,7 +165,17 @@ const style = StyleSheet.create({
     marginBottom:20,
     padding:15,
   },
-
-
-})
+  giftName:{
+      fontWeight:'bold', 
+      fontSize:17, 
+      marginTop:10,
+      textAlign:'center'
+  },
+  giftPrice:{
+      fontWeight:'bold', 
+      fontSize:15, 
+      marginTop:10,
+      textAlign:'center',
+  },
+});
 export default  NewGiftPage
