898bbd404c0d3f29d185f86caa549ba47af73613
NoyAharon1996 <93701572+NoyAharon1996@users.noreply.github.com>
2022-04-08 14:40:19 +0300

    14:40

--- SmarTrash/SmarTrash-React/SmarTrash/src/screens/GiftScreen/GiftPurchase.jsx
+++ SmarTrash/SmarTrash-React/SmarTrash/src/screens/GiftScreen/GiftPurchase.jsx
@@ -1,13 +1,23 @@
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image, Dimensions,TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import COLORS from '../../Consts/colors';
 import { MaterialCommunityIcons,Ionicons } from '@expo/vector-icons';
 
+const apiUrl = 'http://proj.ruppin.ac.il/bgroup91/prod/api/Gift/ShippingDetails/';
 const { width } = Dimensions.get('screen');
-const cardWidth = width / 1.05;
+const cardWidth = width / 1.06;
 
 const GiftPurchase = () => {
+  useEffect( () => {
+
+
+  },[]);
+  const AddNewAdress=()=>{
+
+
+
+  }
   return (
     <View>
       <View style={{ alignSelf: 'center' }}>
@@ -17,7 +27,7 @@ const GiftPurchase = () => {
             source={{ uri: 'https://www.thehandbook.com/cdn-cgi/image/width=300,height=300,fit=cover,q=85/https://files.thehandbook.com/uploads/2019/12/22708923_288175598347572_5346731196820750336_n.jpg' }} />
         </View>
       </View>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-end', top: 75, right: 20 }}>
+      <Text style={{ fontSize: 25, fontWeight: 'bold', color: COLORS.primary, alignSelf: "flex-start", top: 60,paddingRight:20  }}>
         {'כתובת '}
       </Text>
       <View style={style.topHotelCard}>
@@ -29,20 +39,20 @@ const GiftPurchase = () => {
             zIndex: 1,
             flexDirection: 'row',
           }}>
-          <View right={300} top={50}>
+          <View style={{ alignSelf: 'flex-end'}} top={50}>
             <Ionicons name="md-checkmark-circle" size={60} color={COLORS.primary} />
             
           </View>
         </View>
         <View style={{ paddingVertical: 3, paddingHorizontal: 10 }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-end', margin: 10 }}>
-            {'כתובת 1'}
+          <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 10 }}>
+            {'כתובת ברירת מחדל'}
           </Text>
-          <Text style={{ fontSize: 15, fontWeight: 'bold', alignSelf: 'flex-end', margin: 5 }}>{"יוספטל 3"}</Text>
-          <Text style={{ fontSize: 12, fontWeight: 'bold', color: COLORS.grey, alignSelf: 'flex-end', margin: 5 }}>
+          <Text style={{ fontSize: 18, fontWeight: 'bold', alignSelf: 'flex-start', margin: 5 }}>{"יוספטל 3"}</Text>
+          <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.grey, alignSelf: 'flex-start', margin: 5 }}>
             {"חיפה"}
           </Text>
-          <Text style={{ fontSize: 12, fontWeight: 'bold', color: COLORS.grey, alignSelf: 'flex-end', margin: 5 }}>
+          <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.grey, alignSelf: 'flex-start', margin: 5 }}>
             {"052-9589748"}
           </Text>
         </View>
@@ -58,15 +68,17 @@ const GiftPurchase = () => {
             zIndex: 1,
             flexDirection: 'row',
           }}>
-          <View right={300} top={5}>
+          <View  style={{ alignSelf: 'flex-end'}} top={5}>
           <MaterialCommunityIcons name="plus-circle" size={50} color={COLORS.primary}/>
           </View>
         </View>
+        <TouchableOpacity onPress={AddNewAdress}>
         <View style={{ paddingVertical: 3, paddingHorizontal: 10}}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-end', margin:20,}}>
-            {'כתובת 1'} 
+          <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin:20,}}>
+            {'הוסף כתובת'} 
           </Text>
         </View>
+        </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -74,10 +86,11 @@ const GiftPurchase = () => {
 }
 const style = StyleSheet.create({
   profileImage: {
-    width: 50,
-    height: 50,
+    width: 80,
+    height: 80,
     top: 60,
-    left: 150,
+  
+    alignSelf: 'flex-end',
     borderRadius: 100,
     overflow: 'hidden',
   },
