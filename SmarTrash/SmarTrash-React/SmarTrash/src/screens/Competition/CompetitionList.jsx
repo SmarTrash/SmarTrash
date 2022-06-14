1e1109b4e0d2f203eb682936068a558a27f9015e
NogaBob <93701569+NogaBob@users.noreply.github.com>
2022-06-14 11:11:36 +0300

    tikunim

--- SmarTrash/SmarTrash-React/SmarTrash/src/screens/Competition/CompetitionList.jsx
+++ SmarTrash/SmarTrash-React/SmarTrash/src/screens/Competition/CompetitionList.jsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, SafeAreaView, Image, Dimensions,FlatList, Animated, } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, Image, Dimensions, FlatList, Animated, } from 'react-native'
 import React, { useContext, useEffect, useState } from 'react'
 import COLORS from '../../Consts/colors';
 import CoinIcon from '../../Components/Icon/CoinIcon';
@@ -7,26 +7,29 @@ import CompetitionCard from '../../Components/CompetitionCard/CompetitionCard';
 import { GlobalContext } from '../../../GlobalContext/GlobalContext'
 
 const apiUrl = 'http://proj.ruppin.ac.il/bgroup91/prod/api/Competition/GetListOfUsersInMyCity';
-
 const { width } = Dimensions.get('screen');
 const cardWidth = width / 1.06;
 
-const CompetitionList=()=> {
+
+
+const CompetitionList = (navigation, route) => {
+  const { userImg } = useContext(GlobalContext);
   const [usersPlaces, setUsersPlaces] = useState('');
-  const {userEmail} = useContext(GlobalContext);
- 
+  const { userEmail } = useContext(GlobalContext);
+
   useEffect(() => {
 
     fetch(apiUrl, {
       method: 'POST',
-      body: JSON.stringify({UserEmail:userEmail}),
+      body: JSON.stringify({ UserEmail: userEmail }),
       headers: new Headers({
         'Content-type': 'application/json; charset=UTF-8',
         'Accept': 'application/json; charset-UTF-8'
 
       })
     }).then(response => { return response.json() })
-      .then(data => {   console.log('gbgfgvf',data)
+      .then(data => {
+        console.log('gbgfgvf', data)
         // data.map(st => setUsersPlaces(st))
         setUsersPlaces(data);
 
@@ -34,188 +37,88 @@ const CompetitionList=()=> {
   }
     , []);
 
-    console.log('usersPlaces',usersPlaces)
+  console.log('usersPlaces', usersPlaces)
   return (
-    // <View style={{ backgroundColor: COLORS.white, flex: 1 }}>
-    //   <View style={style.titleContainer}>
-    //     <View style={style.profileImage}>
-    //       <Image
-    //         style={style.image}
-    //         source={{ uri: 'https://www.thehandbook.com/cdn-cgi/image/width=300,height=300,fit=cover,q=85/https://files.thehandbook.com/uploads/2019/12/22708923_288175598347572_5346731196820750336_n.jpg' }} />
-    //     </View>
-    //     <View style={style.txtTitleContainer}>
-    //       <Text style={style.txtTitle}>{'המתחרים בעיר שלך'}</Text>
-          
-    //     </View> 
-    //   </View>
-    //   <View>
-    
-      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
-       
-      <View style={style.titleContainer}>
-        <View style={[style.profileImage,{ paddingBottom: 15 }]}>
-         
+
+    <View style={{ flex: 1, backgroundColor: COLORS.white, }}>
+
+
+      <View style={{ alignSelf: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        <View style={style.profileImage}>
           <Image
             style={style.image}
-            source={{ uri: 'https://www.thehandbook.com/cdn-cgi/image/width=300,height=300,fit=cover,q=85/https://files.thehandbook.com/uploads/2019/12/22708923_288175598347572_5346731196820750336_n.jpg' }} />
-         </View>
-         <View style={style.txtTitleContainer}>
-         <Text style={style.txtTitle}>
-          המתחרים בעיר שלך
-          </Text>
+            source={{ uri: userImg }} />
         </View>
+        <Text style={{ fontSize: 25, fontWeight: 'bold', color: COLORS.primary, top: 60, margin: 5 }}>
+          המתחרים בעיר שלך
+        </Text>
       </View>
-      {/* <ScrollView showsVerticalScrollIndicator={false}> */}
-          <FlatList
-            data={usersPlaces}
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{
-              paddingLeft: 20,
-              marginTop: 20,
-              paddingBottom: 30,
-            }}
-            renderItem={({ item,i }) => <CompetitionCard usersPlaces={item} index={i} />}
-          />
-        {/* <Animated.FlatList
-          onMomentumScrollEnd={(e) => {
-            setActiveCardIndex(
-              Math.round(e.nativeEvent.contentOffset.x / cardWidth),
-            );
-          }}
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-            { useNativeDriver: true },
-          )}
-          
+
+      <View style={{top:80}}>
+        <FlatList
           data={usersPlaces}
-          contentContainerStyle={{
-            paddingVertical: 50,
-            alignItems:'center',
-            alignContent:'center', 
-            alignSelf:'center',
-            // justifyContent:'center', 
-            // paddingLeft: 10,
-            // paddingRight: cardWidth / 2 - 40,
-          }}
           showsHorizontalScrollIndicator={false}
-          renderItem={({ item, index }) => <CompetitionCard usersPlaces={item} index={index} />}
-          snapToInterval={cardWidth}
-        /> */}
-      {/* </ScrollView> */}
-    </SafeAreaView>
+          contentContainerStyle={{
+            paddingLeft: 20,
+            marginTop: 20,
+            paddingBottom: 30,
+          }}
+          renderItem={({ item, i }) =>
+            <CompetitionCard usersPlaces={item} index={i} />}
+        />
+      </View>
+
+
+    </View>
+
+
+    // {/* <ScrollView showsVerticalScrollIndicator={false}> */}
+
+    // {/* <Animated.FlatList
+    //     onMomentumScrollEnd={(e) => {
+    //       setActiveCardIndex(
+    //         Math.round(e.nativeEvent.contentOffset.x / cardWidth),
+    //       );
+    //     }}
+    //     onScroll={Animated.event(
+    //       [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+    //       { useNativeDriver: true },
+    //     )}
+
+    //     data={usersPlaces}
+    //     contentContainerStyle={{
+    //       paddingVertical: 50,
+    //       alignItems:'center',
+    //       alignContent:'center', 
+    //       alignSelf:'center',
+    //       // justifyContent:'center', 
+    //       // paddingLeft: 10,
+    //       // paddingRight: cardWidth / 2 - 40,
+    //     }}
+    //     showsHorizontalScrollIndicator={false}
+    //     renderItem={({ item, index }) => <CompetitionCard usersPlaces={item} index={index} />}
+    //     snapToInterval={cardWidth}
+    //   /> */}
+    // {/* </ScrollView> */}
+
   )
-}
+};
+
 export default CompetitionList;
 const style = StyleSheet.create({
 
-  txtTitle: {
-    fontFamily: 'HelveticaNeue',
-    color: '#52575D',
-    fontSize: 24,
-    fontWeight: 'bold',
-    top: 50,
-    textAlign: 'center',
-  },
   profileImage: {
-    width: 60,
-    height: 60,
+    width: 80,
+    height: 80,
     top: 60,
+    alignSelf: 'center',
     borderRadius: 100,
     overflow: 'hidden',
-    marginRight: 15,
   },
-
   image: {
     flex: 1,
     width: undefined,
     height: undefined,
   },
- 
-  titleContainer: {
-    flexDirection: 'row-reverse',
-    marginBottom: 20,
-  },
-  txtTitleContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignContent: 'flex-end',
-  },
-  // txtCard: {
-  //   fontFamily: 'HelveticaNeue',
-  //   color: '#52575D',
-  //   fontSize: 16,
-  //   fontWeight: 'bold',
-  //   margin: 10,
-  // },
-  // pointIcon: {
-  //   marginTop: 8,
-  // },
-  // header: {
-  //   marginTop: 20,
-  //   flexDirection: 'row',
-  //   justifyContent: 'space-between',
-  //   paddingHorizontal: 20,
-  // },
- 
-  
-  // card: {
-  //   height: 280,
-  //   width: cardWidth,
-  //   elevation: 15,
-  //   marginRight: 20,
-  //   borderRadius: 15,
-  //   backgroundColor: COLORS.white,
-  // },
-  // cardImage: {
-  //   height: 200,
-  //   width: '100%',
-  //   borderTopLeftRadius: 15,
-  //   borderTopRightRadius: 15,
-  // },
-  // priceTag: {
-  //   height: 60,
-  //   width: 80,
-  //   backgroundColor: COLORS.primary,
-  //   position: 'absolute',
-  //   zIndex: 1,
-  //   right: 0,
-  //   borderTopRightRadius: 15,
-  //   borderBottomLeftRadius: 15,
-  //   justifyContent: 'center',
-  //   alignItems: 'center',
-  // },
-  // cardDetails: {
-  //   height: 100,
-  //   borderRadius: 50,
-  //   backgroundColor: COLORS.white,
-  //   position: 'absolute',
-  //   bottom: 0,
-  //   padding: 20,
-  //   width: cardWidth,
-  // },
-  // cardOverLay: {
-  //   height: 280,
-  //   backgroundColor: COLORS.white,
-  //   position: 'absolute',
-  //   zIndex: 100,
-  //   width: cardWidth,
-  //   borderRadius: 15,
-  // },
-
-  // topHotelCard: {
-  //   height: 100,
-  //   width: cardWidth,
-  //   backgroundColor: 'black',
-  //   margin: 10,
-  //   borderRadius: 10,
-  //   elevation: 15,
-  //   shadowColor: '#171717',
-  //   shadowOffset: { width: -2, height: 4 },
-  //   shadowOpacity: 0.3,
-  //   shadowRadius: 8,
-  //   flexDirection: 'row-reverse',
-  //   alignItems: 'center',
-  // },
-
 
 });
--- SmarTrash/SmarTrash-React/SmarTrash/src/screens/GiftScreen/GiftPurchase.jsx
+++ SmarTrash/SmarTrash-React/SmarTrash/src/screens/GiftScreen/GiftPurchase.jsx
@@ -41,116 +41,99 @@ const GiftPurchase = ({ navigation, route }) => {
   return (
     <View style={style.container}>
 
-      <View style={{ alignSelf: 'center' }}>
+      <View style={{ alignSelf: 'center', alignItems: 'center', flexDirection: 'column' }}>
         <View style={style.profileImage}>
           <Image
             style={style.image}
             source={{ uri: userImg }} />
         </View>
+        <Text style={{ fontSize: 25, fontWeight: 'bold', color: COLORS.primary, top: 60, margin: 5 }}>
+          כתובת
+        </Text>
       </View>
-      <Text style={{ fontSize: 25, fontWeight: 'bold', color: COLORS.primary, alignSelf: "flex-start", top: 60, paddingRight: 20 }}>
-        {'כתובת '}
-      </Text>
-      <View style={style.topHotelCard}>
-        <View
-          style={{
-            position: 'absolute',
-            top: 5,
-            right: 5,
-            zIndex: 1,
-            flexDirection: 'row',
-          }}>
-          <View style={{ alignSelf: 'flex-end' }} top={50}>
-            <Ionicons name="md-checkmark-circle" size={60} color={COLORS.primary} />
 
+      <View style={style.AdressCard}>
+
+        <View style={style.AdressCardContent}>
+          <Ionicons style={style.icon} name="md-checkmark-circle" size={60} color={COLORS.primary} />
+          <View style={style.txtcontainer}>
+            <Text style={style.txtTitle}>
+              {'כתובת ברירת מחדל'}
+            </Text>
+            <Text style={[style.txt, { fontSize: 18, color: COLORS.dark }]}>{userShippingDetails.StreetNameAndNumber}</Text>
+            <Text style={style.txt}>
+              {userShippingDetails.city}
+            </Text>
+            <Text style={style.txt}>
+              {userShippingDetails.Phone}
+            </Text>
           </View>
         </View>
-        <View style={{ paddingVertical: 3, paddingHorizontal: 10 }}>
-          <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 10 }}>
-            {'כתובת ברירת מחדל'}
+
+      </View>
+
+      {/* הוספת כתובת */}
+
+      <View style={style.AddNewAddress}>
+        <View style={style.AddNewAddressContent}>
+          <Text style={style.txtTitleAddAddress}>
+            הוסף כתובת
           </Text>
-          <Text style={{ fontSize: 18, fontWeight: 'bold', alignSelf: 'flex-start', margin: 5 }}>{userShippingDetails.StreetNameAndNumber}</Text>
-          <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.grey, alignSelf: 'flex-start', margin: 5 }}>
-            {userShippingDetails.city}
-          </Text>
-          <Text style={{ fontSize: 15, fontWeight: 'bold', color: COLORS.grey, alignSelf: 'flex-start', margin: 5 }}>
-            {userShippingDetails.Phone}
-          </Text>
+          <MaterialCommunityIcons style={style.iconAddAdress} name="plus-circle" size={50} color={COLORS.primary} />
+          {/* <TouchableOpacity onPress={AddNewAdress}> */}
+          {/* </TouchableOpacity> */}
         </View>
 
-        {/* הוספת כתובת */}
-
-        <View style={style.AddNewAddress}>
-          <View
-            style={{
-              position: 'absolute',
-              top: 5,
-              right: 5,
-              zIndex: 1,
-              flexDirection: 'row',
-            }}>
-            <View style={{ alignSelf: 'flex-end' }} top={5}>
-              <MaterialCommunityIcons name="plus-circle" size={50} color={COLORS.primary} />
-            </View>
-          </View>
-          <TouchableOpacity onPress={AddNewAdress}>
-            <View style={{ paddingVertical: 3, paddingHorizontal: 10 }}>
-              <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20, }}>
-                {'הוסף כתובת'}
-              </Text>
-            </View>
-          </TouchableOpacity>
-
-          {/* סה"כ נקודות - טקסט*/}
-          <View style={{ top: 20 }}>
-            <View>
-              <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20 }}>
-                {'סה"כ נקודות'}
-              </Text>
-            </View>
-            {/* {'סה"כ נקודות'} */}
-            <View>
-              <Text style={[style.priceTag, { fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20 }]}>
-                {userShippingDetails.points} <CoinIcon />
-              </Text>
-            </View>
-          </View>
-
-          {/* מחיר הטבה - טקסט*/}
-          <View style={{ bottom: 95 }}>
-            <View>
-              <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20 }}>
-                {'מחיר הטבה'}
-              </Text>
-            </View>
-            {/* {'מחיר הטבה'} */}
-            <View>
-              <Text style={[style.priceTag, { fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20 }]}>
-                {userShippingDetails.price} <CoinIcon />
-              </Text>
-
-              <CustonButton
-                text='רכישה'
-                onPress={() => { navigation.navigate('ApprovedPurchase') }}
-              />
-
-            </View>
-          </View>
+      </View>
+      {/* סה"כ נקודות - טקסט*/}
+      <View style={{ top: 20 }}>
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20 }}>
+            {'סה"כ נקודות'}
+          </Text>
+        </View>
+        {/* {'סה"כ נקודות'} */}
+        <View>
+          <Text style={[style.priceTag, { fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20 }]}>
+            {userShippingDetails.points} <CoinIcon />
+          </Text>
         </View>
       </View>
+
+      {/* מחיר הטבה - טקסט*/}
+      <View style={{ bottom: 95 }}>
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20 }}>
+            {'מחיר הטבה'}
+          </Text>
+        </View>
+        {/* {'מחיר הטבה'} */}
+        <View>
+          <Text style={[style.priceTag, { fontSize: 20, fontWeight: 'bold', color: COLORS.primary, alignSelf: 'flex-start', margin: 20 }]}>
+            {userShippingDetails.price} <CoinIcon />
+          </Text>
+
+          <CustonButton
+            text='רכישה'
+            onPress={() => { navigation.navigate('ApprovedPurchase') }}
+          />
+
+        </View>
+      </View>
+
+
     </View>
   )
 }
 const style = StyleSheet.create({
   container: {
-    backgroundColor:COLORS.white,
+    backgroundColor: COLORS.white,
   },
   profileImage: {
     width: 80,
     height: 80,
     top: 60,
-
-    alignSelf: 'flex-end',
+    alignSelf: 'center',
     borderRadius: 100,
     overflow: 'hidden',
   },
@@ -159,16 +142,50 @@ const style = StyleSheet.create({
     width: undefined,
     height: undefined,
   },
-  topHotelCard: {
-    height: 180,
+
+  AdressCard: {
+    height: 120,
     width: cardWidth,
     backgroundColor: COLORS.white,
     elevation: 15,
     marginHorizontal: 10,
     borderRadius: 10,
-    top: 80,
+    top: 100,
+    shadowColor: '#171717',
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    //flexDirection:'row',
+  },
+  AdressCardContent: {
+    margin: 5,
+    alignSelf: 'baseline',
+    flexDirection: 'row-reverse',
+  },
+  icon: {
+    marginLeft: 120,
+    alignSelf: 'flex-end',
+    marginBottom: 15,
+  },
+  txtcontainer: {
+    flexDirection: 'column',
+    margin: 10
+  },
+
+  txtTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: COLORS.primary,
+    alignSelf: 'flex-start',
+  },
+  txt: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: COLORS.grey,
+    alignSelf: 'flex-start',
 
   },
+
   AddNewAddress: {
     height: 70,
     width: cardWidth,
@@ -176,20 +193,30 @@ const style = StyleSheet.create({
     elevation: 15,
     marginHorizontal: 10,
     borderRadius: 10,
-    top: 80,
-    right: 10,
+    top: 200,
+    shadowColor: '#171717',
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  AddNewAddressContent:{
+    flexDirection: 'row',
+  },
+  txtTitleAddAddress: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: COLORS.primary,
+    alignSelf: 'flex-start',
+    margin: 20,
+  },
+  iconAddAdress: {
+    marginLeft: 160,
+    alignSelf: 'flex-end',
+  },
 
-  },
-  topHotelCardImage: {
-    height: 250,
-    width: cardWidth,
-    borderTopRightRadius: 10,
-    borderTopLeftRadius: 10,
-  },
   priceTag: {
     height: 40,
     alignItems: 'center',
-
     bottom: 65,
     width: 150,
     left: 100,
